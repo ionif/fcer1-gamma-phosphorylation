a24688efcf877a3a65c21388e06f2ca54ca0f808
Alex <alex@Alexs-Air.newmexicoconsortium.org>
2016-06-21 08:52:47 -0600

    added legit both data with avgs

---
+++ base+both/data/7e-4/10/avg2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+DATADIR=$HOME/ork/src/github.com/user/fcer1-gamma-phosphorylation/base+both/data/7e-4/10
+cat $DATADIR/*/*.gdat | grep -v '#' | awk '{cnt[$1]++; p0[$1]+=$2; p1[$1]+=$3; p2[$1]+=$4; p3[$1]+=$5; p4[$1]+=$6; p5[$1]+=$7; p6[$1]+=$8; p7[$1]+=$9; p8[$1]+=$10} END { for(i in cnt) print i,p0[i]/cnt[i],p1[i]/cnt[i],p2[i]/cnt[i],p3[i]/cnt[i],p4[i]/cnt[i],p5[i]/cnt[i],p6[i]/cnt[i],p7[i]/cnt[i],p8[i]/cnt[i] }' | sort -g > $DATADIR/avg.gdat
